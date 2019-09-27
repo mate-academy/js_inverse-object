e011df86571239f05b4286c3b7ece83e28afdd09
Karpyshyn Roman <karpyshyn.roman@gmail.com>
2019-09-27 12:11:18 +0300

    colution

--- src/invertObject.js
+++ src/invertObject.js
@@ -19,7 +19,15 @@
  * @return {Object}
  */
 function invertObject(items) {
-  // write code here
+  const invertedObject = {};
+
+  for (const key in items) {
+    if (invertedObject.hasOwnProperty(items[key])) {
+      return null;
+    }
+    invertedObject[items[key]] = key;
+  }
+  return invertedObject;
 }
 
 module.exports = invertObject;
