78cbb5ed5ef39aed53897c7658e7df74db742915
nazmeln <nazik100378@gmail.com>
2020-05-26 00:06:11 +0300

    solved

--- src/invertObject.js
+++ src/invertObject.js
@@ -20,6 +20,17 @@
  */
 function invertObject(items) {
   // write code here
+  const newObject = {};
+
+  for (const key in items) {
+    if (newObject.hasOwnProperty(items[key])) {
+      return null;
+    } else {
+      newObject[items[key]] = key;
+    }
+  }
+
+  return newObject;
 }
 
 module.exports = invertObject;
